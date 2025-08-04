b77a4658180fdf1fbc9cd025b0a0be1ed4915d52
Paseleon <76861131+Yubikitasu@users.noreply.github.com>
2025-08-04 09:09:47 +0700

    .

--- src/render.js
+++ src/render.js
@@ -2,15 +2,15 @@ const video = document.querySelector('video')
 
 // Initialize Firebase
 
-navigator.mediaDevices.getDisplayMedia({
-    audio: false,
-    video: {
-        frameRate: 30
-    }
-}).then(stream => {
-    video.srcObject = stream
-    video.onloadedmetadata = (e) => video.play()
-}).catch(e => console.log(e))
+// navigator.mediaDevices.getDisplayMedia({
+//     audio: false,
+//     video: {
+//         frameRate: 30
+//     }
+// }).then(stream => {
+//     video.srcObject = stream
+//     video.onloadedmetadata = (e) => video.play()
+// }).catch(e => console.log(e))
 
 // stopButton.addEventListener('click', () => {
 //   video.pause()
